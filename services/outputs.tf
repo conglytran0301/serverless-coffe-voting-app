52d844626b299e5ced33b5375dacb11d153d7f2e
conglytran0301 <congly.work@gmail.com>
2024-09-30 19:12:46 +0700

    first commit

---
+++ services/outputs.tf
@@ -0,0 +1,19 @@
+# --------------------------------------------------------- 
+# Module 2 - Backend APIs
+# ---------------------------------------------------------
+
+output "apigw_endpoint" {
+  value = module.api_gateway.apigatewayv2_api_api_endpoint
+}
+
+# ---------------------------------------------------------
+# Module 4 - Realtime Updates
+# ---------------------------------------------------------
+
+# output "iotcore_endpoint" {
+#   value = data.aws_iot_endpoint.current.endpoint_address
+# }
+
+# output "cognito_identity_pool_id" {
+#   value = aws_cognito_identity_pool.main.id
+# }
